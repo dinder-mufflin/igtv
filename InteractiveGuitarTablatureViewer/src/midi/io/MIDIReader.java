388789c5afda53ca59efea2f00158c13c7b674d0
Jonn Spencer <blatherings@live.com>
2014-03-12 00:15:57 -0400

    Added MIDIReader and MIDIWriter classes.  KaraokeMidiJava jar added and referenced.

---
+++ InteractiveGuitarTablatureViewer/src/midi/io/MIDIReader.java
@@ -0,0 +1,55 @@
+package midi.io;
+
+import edu.columbia.ee.csmit.MidiKaraoke.read.PianoRoll;
+import edu.columbia.ee.csmit.MidiKaraoke.read.PianoRollViewParser;
+import structures.Score;
+
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import java.io.File;
+
+public class MIDIReader {
+
+	/**
+	 * Returns the notes from a MIDI file as a two-dimensional double array.
+	 * Columns are pitch, velocity, channel, start (ticks), duration (ticks),
+	 * start (seconds), duration (seconds), and track
+	 * 
+	 * @param fileLocation
+	 *            Location of the MIDI file
+	 * @param sortNotes
+	 *            Whether notes should be returned as sorted.
+	 * @return
+	 */
+	public static Score readScore(String fileLocation) {
+
+		File midiFile = new File(fileLocation);
+
+		if (!midiFile.exists()) {
+			return null;
+		}
+
+		try {
+
+			Sequence seq = MidiSystem.getSequence(midiFile);
+
+			int ticksPerQuarterNotes = seq.getResolution();
+
+			PianoRoll roll = PianoRollViewParser.parse(seq, false);
+
+			// pitch, velocity, channel, onset (ticks), duration (ticks), onset
+			// (seconds), duration (seconds), track
+			double[][] notes = roll.getNotesDoubles();
+
+			return new Score(notes, ticksPerQuarterNotes);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
+}
---
+++ InteractiveGuitarTablatureViewer/src/structures/Score.java
@@ -0,0 +1,14 @@
+package structures;
+
+public class Score {
+
+	public double[][] notes;
+
+    public double ticksPerQuarterNotes;
+
+	public Score(double[][] notes, double ticksPerQuarterNotes) {
+		this.notes = notes;
+        this.ticksPerQuarterNotes = ticksPerQuarterNotes;
+	}
+
+}
