77b7a07a7aca729084c51d2933b8d70a0ebc8376
Jonn Spencer <blatherings@live.com>
2014-03-12 21:33:33 -0400

    Edited a comment.

--- InteractiveGuitarTablatureViewer/src/midi/io/MIDIReader.java
+++ InteractiveGuitarTablatureViewer/src/midi/io/MIDIReader.java
@@ -15,10 +15,8 @@ public class MIDIReader {
 	 * Columns are pitch, velocity, channel, start (ticks), duration (ticks),
 	 * start (seconds), duration (seconds), and track
 	 * 
-	 * @param fileLocation
-	 *            Location of the MIDI file
-	 * @param sortNotes
-	 *            Whether notes should be returned as sorted.
+	 * @param fileLocation Location of the MIDI file
+	 * @param sortNotes Whether notes should be returned as sorted.
 	 * @return
 	 */
 	public static Score readScore(String fileLocation) {
