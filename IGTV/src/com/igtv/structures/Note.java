69d3cc8dd90d7fe6c432825da8b1769cf0dd6093
jcgmusic1 <jcgmusic1@gmail.com>
2014-03-30 11:23:17 -0400

    Fixing error

--- IGTV/src/com/igtv/structures/Note.java
+++ IGTV/src/com/igtv/structures/Note.java
@@ -5,69 +5,78 @@ package com.igtv.structures;
  */
 public class Note {
 
-  /**
-   * Onset of the note (in ticks)
-   */
-  private int onsetInTicks;
+	Note(long onsetInTicks, long durationInTicks, int pitch, int channel,
+			int track) {
+		this.onsetInTicks = onsetInTicks;
+		this.durationInTicks = durationInTicks;
+		this.pitch = pitch;
+		this.channel = channel;
+		this.track = track;
+	}
 
-  /**
-   * Duration of the note (in ticks)
-   */
-  private int durationInTicks;
+	/**
+	 * Onset of the note (in ticks)
+	 */
+	private long onsetInTicks;
 
-  /**
-   * MIDI pitch (60 is Middle C, 61 is C#, etc.)
-   */
-  private int pitch;
+	/**
+	 * Duration of the note (in ticks)
+	 */
+	private long durationInTicks;
 
-  /**
-   * Channel of the note from the imported MIDI file
-   */
-  private int channel;
+	/**
+	 * MIDI pitch (60 is Middle C, 61 is C#, etc.)
+	 */
+	private int pitch;
 
-  /**
-   * Track of the note from the imported MIDI file
-   */
-  private int track;
+	/**
+	 * Channel of the note from the imported MIDI file
+	 */
+	private int channel;
 
-  public int getOnsetInTicks() {
-    return onsetInTicks;
-  }
+	/**
+	 * Track of the note from the imported MIDI file
+	 */
+	private int track;
 
-  public void setOnsetInTicks(int onsetInTicks) {
-    this.onsetInTicks = onsetInTicks;
-  }
+	public long getOnsetInTicks() {
+		return onsetInTicks;
+	}
 
-  public int getDurationInTicks() {
-    return durationInTicks;
-  }
+	public void setOnsetInTicks(int onsetInTicks) {
+		this.onsetInTicks = onsetInTicks;
+	}
 
-  public void setDurationInTicks(int durationInTicks) {
-    this.durationInTicks = durationInTicks;
-  }
+	public long getDurationInTicks() {
+		return durationInTicks;
+	}
 
-  public int getPitch() {
-    return pitch;
-  }
+	public void setDurationInTicks(int durationInTicks) {
+		this.durationInTicks = durationInTicks;
+	}
 
-  public void setPitch(int pitch) {
-    this.pitch = pitch;
-  }
+	public int getPitch() {
+		return pitch;
+	}
 
-  public int getChannel() {
-    return channel;
-  }
+	public void setPitch(int pitch) {
+		this.pitch = pitch;
+	}
 
-  public void setChannel(int channel) {
-    this.channel = channel;
-  }
+	public int getChannel() {
+		return channel;
+	}
 
-  public int getTrack() {
-    return track;
-  }
+	public void setChannel(int channel) {
+		this.channel = channel;
+	}
 
-  public void setTrack(int track) {
-    this.track = track;
-  }
+	public int getTrack() {
+		return track;
+	}
+
+	public void setTrack(int track) {
+		this.track = track;
+	}
 
 }
--- IGTV/src/com/igtv/structures/Score.java
+++ IGTV/src/com/igtv/structures/Score.java
@@ -6,60 +6,116 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Track;
 
+import edu.columbia.ee.csmit.MidiKaraoke.read.NotesInMidi;
+import edu.columbia.ee.csmit.MidiKaraoke.read.PianoRoll;
+import edu.columbia.ee.csmit.MidiKaraoke.read.PianoRollViewParser;
+
 /**
- * A Score is a structure that stores notes as an 8-tuple: (pitch, velocity, channel, start (ticks),
- * duration (ticks), start (seconds), duration (seconds), track)
+ * A Score is a structure that stores notes as an 8-tuple: (pitch, velocity,
+ * channel, start (ticks), duration (ticks), start (seconds), duration
+ * (seconds), track)
  * 
- * Serves as a temporary DTO to transmit between the MidiReader and the ScoreAnalyzer.
+ * Serves as a temporary DTO to transmit between the MidiReader and the
+ * ScoreAnalyzer.
  */
 public class Score {
 
-  /**
-   * An 8xN double array representing note tuples.
-   */
-  private double[][] notes;
+	/**
+	 * An 8xN double array representing note tuples.
+	 */
+	private double[][] notes;
 
-  /**
-   * Number of ticks per one quarter note.
-   */
-  private double ticksPerQuarterNotes;
+	/**
+	 * Number of ticks per one quarter note.
+	 */
+	private double ticksPerQuarterNotes;
 
-  private Sequence seq;
+	private NotesInMidi[] notes1D;
 
-  public Score(Sequence seq) {
-    this.seq = seq;
-  }
+	private Sequence seq;
 
-  public int numberOfTracks() {
-    return seq.getTracks().length;
-  }
-  
-  public Score getTrack(int track) {
-    
-    Track t = seq.getTracks()[track];
-    
-    Sequence requested;
-    try {
-      
-      requested = new Sequence(seq.getDivisionType(), seq.getResolution());
-      Track newt = requested.createTrack();
-      for(int i=0; i<t.size(); i++) {
-        newt.add(t.get(i));
-      }
-      
-      return new Score(requested);
-    } catch (InvalidMidiDataException e) {
-      return null;
-    }
-  }
-  
-  public Sequence getSequence() {
-    return seq;
-  }
-  
-  public ArrayList<Note> getNotes() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	public Score(Sequence seq) {
+		this.seq = seq;
 
+		try {
+			PianoRoll roll = PianoRollViewParser.parse(seq);
+			NotesInMidi[] notes = roll.getNotes();
+			double[][] noteArray = roll.getNotesDoubles();
+			this.notes = noteArray;
+			this.notes1D = notes;
+		} catch (Exception e) {
+			System.out.println("Problem!");
+			e.printStackTrace();
+			System.out.println(e.toString());
+		}
+
+	}
+
+	/**
+	 * Total number of tracks in the MIDI file
+	 * 
+	 * @return
+	 */
+	public int numberOfTracks() {
+		return seq.getTracks().length;
+	}
+
+	/**
+	 * Returns content of the specified track
+	 * 
+	 * @param Track
+	 *            The track number being returned
+	 * @return The score of the requested track
+	 */
+	public Score getTrack(int track) {
+ 
+ 		Track t = seq.getTracks()[track];
+ 
+ 		Sequence requested;
+ 		try {
+ 			requested = new Sequence(seq.getDivisionType(), seq.getResolution());
+ 			Track newt = requested.createTrack();
+ 			for (int i = 0; i < t.size(); i++) {
+ 				newt.add(t.get(i));
+ 			}
+ 
+ 			return new Score(requested);
+ 		} catch (InvalidMidiDataException e) {
+ 			return null;
+ 		}
+ 	}
+
+	/**
+	 * Returns the entire MIDI sequence
+	 * 
+	 * @return
+	 */
+	public Sequence getSequence() {
+		return seq;
+	}
+
+	/**
+	 * Converts the double[][] note array into an ArrayList
+	 * 
+	 * @return
+	 */
+	public ArrayList<Note> getNotes() {
+ 		ArrayList<Note> output = new ArrayList<Note>();
+ 		Note note = null;
+ 		
+ 		for (int i = 0; i < this.notes1D.length; i++) {
+ 			// Pull value for the note object
+ 			NotesInMidi test = this.notes1D[i];
+ 			long onset = test.getStartTick();
+ 			long duration = test.getDurationTick();
+ 			int pitch = test.getNote();
+ 			int channel = test.getChannel();
+ 			int track = test.getTrackNumber();
+ 			
+ 			// Create the note and add it to the ArrayList
+ 			note = new Note(onset, duration, pitch, channel, track);
+ 			output.add(note);
+ 		}
+ 		return output;
+ 	}
 }
